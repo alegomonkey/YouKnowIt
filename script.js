20f6927b6766dc687528db5d1e428a5e1b550f8f
Johnny Sylvain <74006840+alegomonkey@users.noreply.github.com>
2021-08-25 15:51:47 +0000

    New Button For H&P question

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const button = document.querySelector('.CFA')
+const HPQ1 = document.querySelector('.P1-Q')
+const HPA1 = document.querySelector('.HPA1')
 
-console.log(button);
+console.log(HPA1);
 
-let buttonActive = false
+let HPA1Active = false
 
-function onClick() {
-  if (buttonActive === false) {
-    button.innerHTML = "Mao Zedong (The Book is 'The Little Red Book')"
-    buttonActive = true
+function HPA1F() {
+  if (HPA1Active === false) {
+    HPA1.innerHTML = "Mao Zedong (The Book is 'The Little Red Book')"
+    HPA1Active = true
   } else {
-    button.innerHTML = "Click For Answer"
-    buttonActive = false
+    HPA1.innerHTML = "Click For Answer"
+    HPA1Active = false
   }
 }
 
-button.addEventListener('click', onClick)
+HPA1.addEventListener('click', HPA1F)
+
+let HPQ1Active = false
+
+function HPQ1F() {
+  if (HPQ1Active === false) {
+    HPQ1.innerHTML = "Click For Question"
+    HPQ1Active = true
+  } else {
+    HPQ1.innerHTML = "Who was: <br> Born in China on the day after   Christmas in 1893 <br> AND <br> Wrote a book that sold  nearly a  billion copies? "
+    HPQ1Active = false
+  }
+}
+
+HPQ1.addEventListener('click', HPQ1F)
+
